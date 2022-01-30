a2fabfc192a5cdb9bccdb6a75ee4e5ee27f18243
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-01-30 22:07:20 +0000

    Implement isAuthenticated in App component to conditionally render Article component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import articles from "../../libs/articles";
 import Article from "../Article";
 import Login from "../Login";
@@ -6,17 +7,22 @@ import Title from "../Title";
 import css from "./App.module.css";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className={css.app}>
       <div className={css.nav}>
         <Title />
         <Login />
       </div>
-      <div>
-        {articles.map((article) => {
-          return <Article article={article} />;
-        })}
-      </div>
+
+      {isAuthenticated && (
+        <div>
+          {articles.map((article) => {
+            return <Article article={article} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
