ca2cb05797e76b60418a7e9134d9a2b418127c86
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-01-30 17:22:22 +0000

    Clean up create-react-app boilerplate

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,23 +6,6 @@ function App() {
   return (
     <div className="App">
       <h1>WikiPigeon</h1>
-      {/*       <article className="post">
-        <h2>{articles[0].title}</h2>
-        {articles[0].paragraphs.map((paragraph) => (
-          <p>{paragraph}</p>
-        ))}
-        <button className="like-button">Like 👍</button>
-        <section className="comment-section">
-          {articles[0].comments.map((comment) => {
-            return (
-              <div className="comment">
-                <h4>{comment.name} says:</h4>
-                <p>{comment.text}</p>
-              </div>
-            );
-          })}
-        </section>
-      </article> */}
       {articles.map((article) => {
         return <Article article={article} />;
       })}
